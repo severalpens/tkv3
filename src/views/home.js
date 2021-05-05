e2daa017070277e6a8a0fe44be5f402130b1c8a3
Paul C <paulcollinsaf@gmail.com>
2021-05-06 01:16:24 +1000

    update home page

--- src/views/home.js
+++ src/views/home.js
@@ -11,9 +11,7 @@ const Home = () => (
       </h6>
       <div>
       <ul>
-        <li>The authentication has been outsourced to Auth0.</li>
         <li>Auth0 set up mainly following this <a href="https://auth0.com/blog/complete-guide-to-react-user-authentication/">tutorial</a></li>
-        <li>Mongodb hasn't been added yet.</li>
         <li>EC2 set up mainly  following <a href="https://www.youtube.com/watch?v=NjYsXuSBZ5U&t=447s">this tutorial</a></li>
         <li>Repo is available at <a href="https://github.com/severalpens/tkv3">https://github.com/severalpens/tkv3</a></li>
       </ul>
