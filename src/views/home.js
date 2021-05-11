95a389b3d8c4efb975ddfdd288d606e6969e5c43
Paul C <paulcollinsaf@gmail.com>
2021-05-11 11:02:39 +1000

    change title

--- src/views/home.js
+++ src/views/home.js
@@ -4,11 +4,8 @@ import { Hero, HomeContent,Calculator } from "../components";
 
 const Home = () => (
   <Fragment>
-    <h1>[M]ERN on EC2</h1>
-    <br/>
-    <h6>
-      This is a React application with an Express API both being served from an AWS EC2 instance.
-      </h6>
+    <h1>Paul Collins</h1>
+    <h4>Software Programmer</h4>
       <div>
       <ul>
         <li>Auth0 set up mainly following this <a href="https://auth0.com/blog/complete-guide-to-react-user-authentication/">tutorial</a></li>
