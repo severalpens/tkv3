158051cb34a6637e63083abc7c0964814a1d9f65
Paul C <paulcollinsaf@gmail.com>
2021-05-11 11:06:38 +1000

    add resume content

--- src/views/home.js
+++ src/views/home.js
@@ -9,10 +9,10 @@ const Home = () => (
       <div>
       <ul>
       <li>For more info visit <a href="https://severalpens.com" target="blank">severalpens.com</a></li>
-      <li>Auth0 set up mainly following this <a href="https://auth0.com/blog/complete-guide-to-react-user-authentication/">tutorial</a></li>
-        <li>EC2 set up mainly  following this <a href="https://www.youtube.com/watch?v=NjYsXuSBZ5U&t=447s">tutorial</a></li>
-        <li>An Auto Scaling Group was setup using <a href="https://docs.aws.amazon.com/autoscaling/ec2/userguide/create-asg-from-instance.html">tutorial</a></li>
-        <li>Repo is available at <a href="https://github.com/severalpens/tkv3">https://github.com/severalpens/tkv3</a></li>
+      <li>Auth0 set up mainly following this <a href="https://auth0.com/blog/complete-guide-to-react-user-authentication/"  target="blank">tutorial</a></li>
+        <li>EC2 set up mainly  following this <a href="https://www.youtube.com/watch?v=NjYsXuSBZ5U&t=447s"  target="blank">tutorial</a></li>
+        <li>An Auto Scaling Group was setup using <a href="https://docs.aws.amazon.com/autoscaling/ec2/userguide/create-asg-from-instance.html"  target="blank">tutorial</a></li>
+        <li>Repo is available at <a href="https://github.com/severalpens/tkv3"  target="blank">https://github.com/severalpens/tkv3</a></li>
       </ul>
     </div>
   </Fragment>
